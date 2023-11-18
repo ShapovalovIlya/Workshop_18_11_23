fa367a86ee611b70248f0f59d2ec15336465d0ec
Ilia Shapovalov <greateman@mail.ru>
2023-11-18 19:30:17 +0300

    Initial Commit

---
+++ Workshop_18_11_23/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Workshop_18_11_23
+//
+//  Created by Илья Шаповалов on 18.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Workshop_18_11_23/Workshop_18_11_23App.swift
@@ -0,0 +1,17 @@
+//
+//  Workshop_18_11_23App.swift
+//  Workshop_18_11_23
+//
+//  Created by Илья Шаповалов on 18.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct Workshop_18_11_23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
